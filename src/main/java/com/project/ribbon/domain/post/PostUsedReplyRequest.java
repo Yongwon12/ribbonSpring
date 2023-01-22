c34148397e9ec3e274f7833ed85fb0c65c46efd3
gim-yong-won <tjaaj159@naver.com>
2023-01-22 18:03:07 +0900

    sport community

--- src/main/java/com/project/ribbon/domain/post/PostUsedReplyRequest.java
+++ src/main/java/com/project/ribbon/domain/post/PostUsedReplyRequest.java
@@ -20,6 +20,7 @@ public class PostUsedReplyRequest {
         private Long usedreplyid;
         private Long inherentcommentsid;
         private Integer isrecomment;
+        private Integer token;
 
 
 }
