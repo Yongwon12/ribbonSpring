71f2ea311e7bc5c2505f4bdf8167d3e74ba17f1d
gim-yong-won <tjaaj159@naver.com>
2023-01-26 14:02:59 +0900

    sport community modify valid

--- src/main/java/com/project/ribbon/domain/post/PostGroupCommentsRequest.java
+++ src/main/java/com/project/ribbon/domain/post/PostGroupCommentsRequest.java
@@ -1,6 +1,7 @@
 package com.project.ribbon.domain.post;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostGroupCommentsRequest {
 
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9]{2,50}$", message = "댓글은 특수문자를 제외한 최소 2자, 최대 50자입니다.")
+        @NotBlank(message="댓글은 필수 입력 값입니다.")
         private String description;
         private Integer userid;
         @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
--- src/main/java/com/project/ribbon/domain/post/PostUsedCommentsRequest.java
+++ src/main/java/com/project/ribbon/domain/post/PostUsedCommentsRequest.java
@@ -1,6 +1,8 @@
 package com.project.ribbon.domain.post;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostUsedCommentsRequest {
 
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9]{2,50}$", message = "댓글은 특수문자를 제외한 최소 2자, 최대 50자입니다.")
+        @NotBlank(message="댓글은 필수 입력 값입니다.")
         private String description;
         private Integer userid;
         private String  nickname;
