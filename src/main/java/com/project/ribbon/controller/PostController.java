1acb61b61dd3b643f709db89e20be4b34845d4fc
gim-yong-won <tjaaj159@naver.com>
2023-02-07 23:21:15 +0900

    push alarm

--- src/main/java/com/project/ribbon/controller/PostController.java
+++ src/main/java/com/project/ribbon/controller/PostController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/project/ribbon/controller/PushAlarmController.java
+++ src/main/java/com/project/ribbon/controller/PushAlarmController.java
@@ -4,6 +4,7 @@ import com.project.ribbon.domain.post.ResponseDTO;
 import com.project.ribbon.service.FirebaseCloudMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,6 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class PushAlarmController {
     private final FirebaseCloudMessageService firebaseCloudMessageService;
-
     @PostMapping("/api/push")
     public ResponseEntity pushMessage(@RequestBody ResponseDTO responseDTO) throws IOException {
         firebaseCloudMessageService.sendMessageTo(
