531534f69851e5d458dab603986dd66e4418a61b
gim-yong-won <tjaaj159@naver.com>
2023-02-25 22:06:10 +0900

    modify admin

--- src/main/java/com/project/ribbon/config/SecurityConfig.java
+++ src/main/java/com/project/ribbon/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.project.ribbon.config;
 import com.project.ribbon.filter.JwtAuthenticationFilter;
 import com.project.ribbon.provide.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,15 +26,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic().disable()
                 .csrf().disable()
+                .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/login").permitAll()
                 .requestMatchers("/api/sign").permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/api/upload").permitAll()
                 .requestMatchers("/api/boardimage/**").permitAll()
                 .requestMatchers("/api/userimage/**").permitAll()
                 .requestMatchers("/api/groupimage/**").permitAll()
@@ -42,20 +43,17 @@ public class SecurityConfig {
                 .requestMatchers("/api/group").hasRole("USER")
                 .requestMatchers("/api/used").hasRole("USER")
                 .requestMatchers("/api/post/**").hasRole("USER")
-                .requestMatchers("/api/realtimeup").hasRole("ADMIN")
-                .requestMatchers("/api/board").hasRole("ADMIN")
-                .requestMatchers("/api/individual").hasRole("ADMIN")
-                .requestMatchers("/api/group").hasRole("ADMIN")
-                .requestMatchers("/api/used").hasRole("ADMIN")
-                .requestMatchers("/api/post/**").hasRole("ADMIN")
-                .requestMatchers("/api/post/**").hasRole("ADMIN")
                 .requestMatchers("/admin/login").permitAll()
+                //.requestMatchers("/admin/report").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/project/ribbon/controller/LoginController.java
+++ src/main/java/com/project/ribbon/controller/LoginController.java
@@ -2,20 +2,19 @@ package com.project.ribbon.controller;
 
 
 import com.project.ribbon.domain.post.MemberLoginRequestDto;
-import com.project.ribbon.domain.post.PostReportUserResponse;
 import com.project.ribbon.dto.TokenInfo;
 import com.project.ribbon.service.MemberService;
 import com.project.ribbon.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.io.IOException;
 
 
 @Controller
@@ -28,25 +27,30 @@ public class LoginController {
     public String showLoginForm() {
         return "admin-login";
     }
-    @PostMapping("/admin/login")
-    public String adminLogin(@RequestBody MemberLoginRequestDto memberLoginRequestDto, HttpServletResponse response) {
-        String userid = memberLoginRequestDto.getUserid();
-        String password = memberLoginRequestDto.getPassword();
-        TokenInfo tokenInfo = memberService.login(userid, password);
-        if (tokenInfo != null) {
-            response.addHeader("Authorization", "Bearer " + tokenInfo.getAccessToken());
-            return "admin-report"; // 헤더 추가 후 admin-report.html 페이지로 이동
-        } else {
-            return "admin-login"; // 로그인 실패 시 다시 로그인 페이지로 이동
-        }
-    }
-
-
     @GetMapping("/admin/report")
-    public String userReport(Model model) {
-        List<PostReportUserResponse> userList = postService.findReportUserAllPost();
-        model.addAttribute("userList", userList);
+    public String showReportForm() {
         return "admin-report";
     }
+//    @GetMapping("/admin/report")
+//    public String userReport(Model model) {
+//        List<PostReportUserResponse> userList = postService.findReportUserAllPost();
+//        model.addAttribute("userList", userList);
+//        return "admin-report";
+//    }
+@PostMapping( "/admin/login")
+public void adminLogin(@RequestBody MemberLoginRequestDto memberLoginRequestDto, HttpServletResponse response) throws IOException {
+    String userid = memberLoginRequestDto.getUserid();
+    String password = memberLoginRequestDto.getPassword();
+    TokenInfo tokenInfo = memberService.login(userid, password);
+    if (tokenInfo != null) {
+        System.out.println(memberLoginRequestDto.getUserid());
+        System.out.println(memberLoginRequestDto.getPassword());
+        System.out.println(tokenInfo.getAccessToken());
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.sendRedirect("/admin/report");
+    } else {
+        response.sendRedirect("/admin/login");
+    }
+}
 
 }
