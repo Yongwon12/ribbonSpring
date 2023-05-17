22be2b7a3d689a996b77b1874f746f777aec1fb8
gim-yong-won <tjaaj159@naver.com>
2023-05-18 04:08:48 +0900

    예외처리 수정

--- src/main/java/com/project/ribbon/controller/ChatController.java
+++ src/main/java/com/project/ribbon/controller/ChatController.java
@@ -3,7 +3,6 @@ package com.project.ribbon.controller;
 import com.project.ribbon.domain.post.PostChatRoomDeleteRequest;
 import com.project.ribbon.domain.post.PostChatRoomResponse;
 import com.project.ribbon.dto.RoomRequest;
-import com.project.ribbon.enums.ExceptionEnum;
 import com.project.ribbon.response.ApiException;
 import com.project.ribbon.service.ChatService;
 import com.project.ribbon.service.PostService;
@@ -25,43 +24,69 @@ public class ChatController {
     private final PostService postService;
 
 //     채팅방 아이디 발급
-    @PostMapping
-    public ResponseEntity<Map<String, Object>> createRoom(@RequestBody RoomRequest roomRequest) {
+@PostMapping
+public ResponseEntity<Map<String, Object>> createRoom(@RequestBody RoomRequest roomRequest) {
+    Map<String, Object> obj = new HashMap<>();
+    try {
         chatService.createRoom(roomRequest.getRoomName(), roomRequest.getMyid(), roomRequest.getYourid());
-        Map<String, Object> obj = new HashMap<>();
         PostChatRoomResponse posts = postService.findPostByMyId(roomRequest.getMyid());
         Map<String, Object> roomInfo = new HashMap<>();
         roomInfo.put("roomId", posts.getRoomid());
         roomInfo.put("roomName", posts.getRoomname());
         obj.put("chatroom", roomInfo);
         return new ResponseEntity<>(obj, HttpStatus.OK);
+    } catch (Exception e) {
+        // Handle the exception here
+        System.err.println("Error occurred while creating room");
+        System.err.println(e.getMessage());
+        obj.put("error", e.getMessage());
+        return new ResponseEntity<>(obj, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+}
         // 특정 채팅방 조회
         @PostMapping("/post/chatroominfo")
-        public ResponseEntity<?> viewChatRoomPost(@RequestBody PostChatRoomResponse myid, Model model) throws ApiException {
-            ExceptionEnum err = ExceptionEnum.RUNTIME_EXCEPTION;
+        public ResponseEntity<?> viewChatRoomPost(@RequestBody PostChatRoomResponse myid, Model model) {
             Map<String, Object> obj = new HashMap<>();
-            List<PostChatRoomResponse> posts = postService.findPostByChatRoomMyId(myid.getMyid());
-            model.addAttribute("posts", posts);
-            obj.put("chatroom", posts);
-            return new ResponseEntity<>(obj, HttpStatus.OK);
+            try {
+                List<PostChatRoomResponse> posts = postService.findPostByChatRoomMyId(myid.getMyid());
+                model.addAttribute("posts", posts);
+                obj.put("chatroom", posts);
+                return new ResponseEntity<>(obj, HttpStatus.OK);
+            } catch (Exception e) {
+                // Handle the exception here
+                System.err.println("Error occurred while fetching posts for chatroom");
+                System.err.println(e.getMessage());
+                obj.put("error", e.getMessage());
+                return new ResponseEntity<>(obj, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         }
     // 특정 채팅내역 조회
     @PostMapping("/post/chatinfo")
-    public ResponseEntity<?> viewChatInfoPost(@RequestBody PostChatRoomResponse roomname, Model model) throws ApiException {
-        ExceptionEnum err = ExceptionEnum.RUNTIME_EXCEPTION;
+    public ResponseEntity<?> viewChatInfoPost(@RequestBody PostChatRoomResponse roomname, Model model) {
         Map<String, Object> obj = new HashMap<>();
-        List<PostChatRoomResponse> posts = postService.findByChatInfoMyIdPost(roomname.getRoomname());
-        model.addAttribute("posts", posts);
-        obj.put("chatinfo", posts);
-        return new ResponseEntity<>(obj, HttpStatus.OK);
+        List<PostChatRoomResponse> posts;
+        try {
+            posts = postService.findByChatInfoMyIdPost(roomname.getRoomname());
+            model.addAttribute("posts", posts);
+            obj.put("chatinfo", posts);
+            return new ResponseEntity<>(obj, HttpStatus.OK);
+        } catch (ApiException e) {
+            // 예외 처리
+            obj.put("err", e.getMessage());
+            return new ResponseEntity<>(obj, HttpStatus.BAD_REQUEST);
+        }
     }
     // 특정 채팅방 삭제
     @DeleteMapping("/post/deleteroom")
-    public ResponseEntity<?> deleteChatRoomPost(@RequestBody PostChatRoomDeleteRequest params) throws ApiException {
-        ExceptionEnum err = ExceptionEnum.RUNTIME_EXCEPTION;
-        postService.deleteChatInfoPost(params);
-        return postService.deleteChatRoomPost(params);
+    public ResponseEntity<?> deleteChatRoomPost(@RequestBody PostChatRoomDeleteRequest params){
+        try {
+            postService.deleteChatInfoPost(params);
+            postService.deleteChatRoomPost(params);
+            return ResponseEntity.ok().build();
+        } catch (ApiException e) {
+            // 예외 처리
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 
