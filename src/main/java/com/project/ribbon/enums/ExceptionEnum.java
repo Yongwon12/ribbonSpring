c365ffcd9da40e7f0f97de8d9ea2fcae09eba9ad
gim-yong-won <tjaaj159@naver.com>
2023-05-27 13:29:50 +0900

    예외처리 수정

--- src/main/java/com/project/ribbon/enums/ExceptionEnum.java
+++ src/main/java/com/project/ribbon/enums/ExceptionEnum.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @ToString
 public enum ExceptionEnum {
-    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, "400"), // 클라이언트 or 서버 쿼리에러
-    ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "401"), // 문법에러
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500"); //서버에러, 요청방식에러
+    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, "400","요청에러"), // 클라이언트 or 서버 쿼리에러
+    ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "401","권한에러"), // 권한 에러
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500","서버에러"); //서버에러, 요청방식에러
 
 
     private final HttpStatus status;
